4d2f51b363c66a0a804fbccbbae6abdd4b21ee02
richardslotala <heutch1976>
2015-12-16 15:08:08 +0100

    php

---
+++ menu.inc.php
@@ -0,0 +1,27 @@
+</div>
+
+<nav class="span4">
+    <h3>MENU</h3>
+
+    <ul>
+        <div class="clearfix">
+
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="article.php">Rédiger un article</a></li>
+            <li><a href="recherche.php">Taper votre recherche </a></li>
+            <li><a href="connect.php">Connexion</a></li>
+
+    </ul>
+    <br/>
+    <br/>
+
+    <div class="clearfix">
+        <label for="Email"> TAPER ICI VOTRE COMMENTAIRE POUR CET ARTICLE : </label> 
+        <div class="input">
+            <input type="text"name="Email" id="Email" value=""/> 
+        </div>
+    </div>
+</nav>
+</div>
+
+</div>
